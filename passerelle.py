4ab5827bb331e19b3a6414e490cde604785dc215
raspout <carellaalexis@gmail.com>
2024-01-12 11:55:11 +0100

    update

--- passerelle.py
+++ passerelle.py
@@ -4,61 +4,71 @@ import time
 import paho.mqtt.client as mqtt
 import requests
 import os
+import json
 from dotenv import load_dotenv
 
 
-PID_MICROBIT = 516
-VID_MICROBIT = 3368
-TIMEOUT = 0.1
-
-
+# connect to the broker
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+    print("mqtt connection result code : "+str(rc))
+    client.subscribe("simulator/SensorEvents")
 
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    client.subscribe("$SYS/#")
-
-# The callback for when a PUBLISH message is received from the server.
+# mqtt message received
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
+    if msg.topic == "simulator/SensorEvents":
+        print("Message reçu du simulateur")
+        message = msg.payload.decode('utf-8')
+        print("event id received via mqtt : " + message)
 
-def find_comport(pid, vid, baud):
-    ''' return a serial port '''
-    ser_port = serial.Serial(timeout=TIMEOUT)
-    ser_port.baudrate = baud
-    ports = list(list_ports.comports())
-    print('scanning ports')
-    for p in ports:
-        print('port: {}'.format(p))
-        try:
-            print('pid: {} vid: {}'.format(p.pid, p.vid))
-        except AttributeError:
-            continue
-        if (p.pid == pid) and (p.vid == vid):
-            print('found target device pid: {} vid: {} port: {}'.format(
-                p.pid, p.vid, p.device))
-            ser_port.port = str(p.device)
-            return ser_port
-    return None
+
+        # json_data = requests.get(API_URL + "/api/passerelle/getEvent?eventid=" + message).text
+        json_data = '[{"sensorID":1,"intensity":5},{"sensorID":2,"intensity":5},{"sensorID":5,"intensity":5},{"sensorID":99,"intensity":5}]'
+        data = json.loads(json_data)
+
+        serial_string = ""
+        for i in range(15):
+            if i < len(data):
+                serial_string = serial_string + str(data[i]["sensorID"]) + " " + str(data[i]["intensity"]) + " "
+            else:
+                serial_string = serial_string + "0 0 "
+        serial_string = serial_string[:-1]
+        print("serial string : " + serial_string)
+        ser.write(serial_string.encode() + b'\n')
+
+
 
 
 if __name__ == '__main__':
     load_dotenv()
 
+    MQTT_BROKER_HOST = os.getenv('MQTT_BROKER_HOST')
+    MQTT_BROKER_PORT = os.getenv('MQTT_BROKER_PORT')
+    MQTT_USERNAME = os.getenv('MQTT_USERNAME')
+    MQTT_PASSWORD = os.getenv('MQTT_PASSWORD')
+    API_URL = os.getenv('API_URL')
+
+
+    print("mqtt host : " + MQTT_BROKER_HOST)
+    print("mqtt port : " + MQTT_BROKER_PORT)
+    print("mqtt username : " + MQTT_USERNAME)
+
+
     client = mqtt.Client()
     client.on_connect = on_connect
     client.on_message = on_message
-    client.connect(os.getenv('MQTT_BROKER_HOST'), int(os.getenv('MQTT_BROKER_PORT')), 60)
+    client.username_pw_set(MQTT_USERNAME, MQTT_PASSWORD)
+    client.connect(MQTT_BROKER_HOST, int(MQTT_BROKER_PORT), 60)
 
     try:
         # Ouvrir le port série
-        ser = find_comport(PID_MICROBIT, VID_MICROBIT, 115200)
-        data = requests.get('ifconfig.me').text
+        ser = serial.Serial(os.getenv('SERIAL_PORT'), int(os.getenv('SERIAL_BAUDRATE')))
+        data = requests.get('https://ifconfig.me').text
 
         # Envoie la chaîne via la liaison série
         ser.write(data.encode())
-
+        print("Envoi de la chaîne : " + data)
+        client.loop_forever()
     except serial.SerialException:
         print(f"Le port série n'a pas pu être ouvert. Assurez-vous que le périphérique est correctement connecté.")
     except KeyboardInterrupt:
